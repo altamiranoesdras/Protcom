215b088effb377f53be9a01c42168091201866ed
Esdras Altamirano <altamiranoesdras@gmail.com>
2016-10-28 21:50:51 -0600

    Archivos iniciales

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#define __LINUX_COM__
+#include "com/serial.h"
+#include <stdio.h>    // Libreria estanda entrada y salida
+#include <unistd.h>   // para utilizar usleep()
+#include <getopt.h>   // para leer la opciones que se pasan como argumentos
+#include <string.h>   // para manejar cadenas de texto
+#include <stdlib.h>
+
+int main() {
+    printf("prueba");
+    system("clear");
+    printf("prueba2");
+    return 0;
+}
